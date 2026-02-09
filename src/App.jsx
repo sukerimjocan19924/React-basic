114b6cb854c2b57c5143ced4884aa8b5f3637add
unknown <hseoglan@naver.com>
2026-02-09 10:25:20 +0900

    ex1더

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import SquareCalc from './components/SquareCalc'
 import SortableList from './components/SortableList'
+import PeopleList from './components/PeopleList'
 
 const App = () => {
   return (
     <div>
+      <PeopleList/>
+      <hr/>
       <SortableList/>
       <hr/>
       <SquareCalc/>
